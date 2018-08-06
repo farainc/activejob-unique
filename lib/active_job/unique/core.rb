30f9a32a51fec76f916ad77b8f70de01c0a1dc84
josh.c <junyu.live@gmail.com>
2018-08-06 04:16:37 -0700

    Revert "Revert "Improve job sorting score.""
    This reverts commit 1908a40ced400cf063c62d797501f3d87ac3b5f9.

--- lib/active_job/unique/core.rb
+++ lib/active_job/unique/core.rb
@@ -67,6 +67,8 @@ module ActiveJob
         end
 
         before_perform do |job|
+          uniqueness_api.progress_stats_initialize(job)
+          
           @uniqueness_progress_stage = PROGRESS_STAGE_PERFORM_ATTEMPTED
           uniqueness_api.incr_progress_stats(job, @uniqueness_progress_stage)
         end
