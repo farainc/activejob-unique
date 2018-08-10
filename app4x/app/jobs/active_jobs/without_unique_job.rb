2f664203ddf8ab4c940945dac1622baf7838c9ab
josh.c <junyu.live@gmail.com>
2018-08-10 13:59:12 -0700

    Update testing jobs.

--- app4x/app/jobs/active_jobs/without_unique_job.rb
+++ app4x/app/jobs/active_jobs/without_unique_job.rb
@@ -8,20 +8,4 @@ class ActiveJobs::WithoutUniqueJob < ApplicationJob
 
     true
   end
-
-  def self.enqueue_multiple
-    total = 1000
-
-    self.prepare_multiple(total)
-
-    (1..total).to_a.shuffle.each do |args|
-      ActiveJobs::EnqueueJob.perform_later(self.name, [args])
-    end
-
-    (1..total).to_a.shuffle.each do |args|
-      ActiveJobs::EnqueueJob.perform_later(self.name, [args])
-    end
-
-    true
-  end
 end
