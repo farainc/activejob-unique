88989a7fe1dc8aacd281f4238839f8f414d26c60
josh.c <junyu.live@gmail.com>
2020-06-11 22:29:44 -0700

    Fix redis gem conn.exists?.

--- lib/active_job/unique/adapters/sidekiq_adapter/web/web_api_logging.rb
+++ lib/active_job/unique/adapters/sidekiq_adapter/web/web_api_logging.rb
@@ -16,7 +16,7 @@ module ActiveJob
 
                   job_stats_all_time.each do |job_name, queues|
                     job_score_key = "#{job_progress_stage_log_key(job_name)}#{PROGRESS_STATS_SEPARATOR}job_score"
-                    next unless conn.exists(job_score_key)
+                    next unless conn.exists?(job_score_key)
 
                     job_log_keys[job_name] = {}
 
@@ -37,7 +37,7 @@ module ActiveJob
                   next_page_availabe = false
 
                   job_score_key = "#{job_progress_stage_log_key(job_name)}#{PROGRESS_STATS_SEPARATOR}job_score"
-                  return [false, []] unless conn.exists(job_score_key)
+                  return [false, []] unless conn.exists?(job_score_key)
 
                   day_score = ensure_job_stage_log_day_base(day)
 
@@ -71,10 +71,10 @@ module ActiveJob
               def query_job_progress_stage_log_job_one(day, job_name, queue_name, uniqueness_id, job_id)
                 Sidekiq.redis_pool.with do |conn|
                   job_score_key = "#{job_progress_stage_log_key(job_name)}#{PROGRESS_STATS_SEPARATOR}job_score"
-                  return { logs: [], args: {} } unless conn.exists(job_score_key)
+                  return { logs: [], args: {} } unless conn.exists?(job_score_key)
 
                   job_log_key = "#{job_progress_stage_log_key(job_name)}#{PROGRESS_STATS_SEPARATOR}job_logs"
-                  return { logs: [], args: {} } unless conn.exists(job_log_key)
+                  return { logs: [], args: {} } unless conn.exists?(job_log_key)
 
                   log_data_key = job_progress_stage_log_key(job_name)
                   log_data_field = "#{sequence_day_score(day)}#{PROGRESS_STATS_SEPARATOR}#{uniqueness_id}"
@@ -132,10 +132,10 @@ module ActiveJob
               def cleanup_job_progress_stage_logs(day, job_name, queue_name = '*', uniqueness_id = '*')
                 Sidekiq.redis_pool.with do |conn|
                   job_score_key = "#{job_progress_stage_log_key(job_name)}#{PROGRESS_STATS_SEPARATOR}job_score"
-                  return unless conn.exists(job_score_key)
+                  return unless conn.exists?(job_score_key)
 
                   job_log_key = "#{job_progress_stage_log_key(job_name)}#{PROGRESS_STATS_SEPARATOR}job_logs"
-                  return unless conn.exists(job_log_key)
+                  return unless conn.exists?(job_log_key)
 
                   day_score = ensure_job_stage_log_day_base(day)
 
@@ -174,10 +174,10 @@ module ActiveJob
               def cleanup_job_progress_stage_log_one(day, job_name, queue_name, uniqueness_id, job_id)
                 Sidekiq.redis_pool.with do |conn|
                   job_score_key = "#{job_progress_stage_log_key(job_name)}#{PROGRESS_STATS_SEPARATOR}job_score"
-                  return unless conn.exists(job_score_key)
+                  return unless conn.exists?(job_score_key)
 
                   job_log_key = "#{job_progress_stage_log_key(job_name)}#{PROGRESS_STATS_SEPARATOR}job_logs"
-                  return unless conn.exists(job_log_key)
+                  return unless conn.exists?(job_log_key)
 
                   job_id_score = conn.zscore(job_score_key, "#{queue_name}:#{uniqueness_id}:#{job_id}").to_f
 
