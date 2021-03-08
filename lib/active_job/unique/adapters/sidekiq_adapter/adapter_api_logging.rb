ed35c8b73d0ab73178d2b09ff8e8f3827dea8f5f
josh.c <junyu.live@gmail.com>
2021-03-08 15:38:55 +0800

    Fix Job logging with limits.

--- lib/active_job/unique/adapters/sidekiq_adapter/adapter_api_logging.rb
+++ lib/active_job/unique/adapters/sidekiq_adapter/adapter_api_logging.rb
@@ -41,15 +41,15 @@ module ActiveJob
                 job_id_value = "#{queue_name}:#{uniqueness_id}:#{job_id}"
                 job_id_score = conn.zscore(job_score_key, job_id_value).to_f
 
+                day_score = ensure_job_stage_log_day_base(day)
+
+                queue_id_score = incr_progress_stage_log_id_score(conn, job_score_key, 'queue', queue_name)
+                queue_id_score = ensure_job_stage_log_queue_id_base(queue_id_score)
+
+                uniqueness_id_score = incr_progress_stage_log_id_score(conn, job_score_key, 'uniqueness_id', uniqueness_id)
+                uniqueness_id_score = ensure_job_stage_log_uniqueness_id_base(uniqueness_id_score)
+
                 if job_id_score.zero?
-                  day_score = ensure_job_stage_log_day_base(day)
-
-                  queue_id_score = incr_progress_stage_log_id_score(conn, job_score_key, 'queue', queue_name)
-                  queue_id_score = ensure_job_stage_log_queue_id_base(queue_id_score)
-
-                  uniqueness_id_score = incr_progress_stage_log_id_score(conn, job_score_key, 'uniqueness_id', uniqueness_id)
-                  uniqueness_id_score = ensure_job_stage_log_uniqueness_id_base(uniqueness_id_score)
-
                   # time_score with timezone
                   now = Time.now.in_time_zone(ActiveJob::Unique::Stats.timezone)
                   time_score = ((now - now.to_date.in_time_zone(ActiveJob::Unique::Stats.timezone)) / 10).to_i
@@ -64,8 +64,33 @@ module ActiveJob
                 job_log_score = job_id_score + progress_stage_score
                 conn.zadd(job_log_key, [job_log_score, job_log_value], nx: true)
 
-                # remove over liimts debug info
-                conn.zremrangebyrank(job_log_key, 0, (0-debug_limits))
+                # remove over limits logs
+                min_score = day_score
+                max_score = min_score + DAY_SCORE_BASE
+
+                loop do
+                  job_score_logs = conn.zrevrangebyscore(
+                    job_score_key,
+                    "(#{max_score}",
+                    min_score,
+                    limit: [debug_limits, debug_limits + 100 + 1]
+                  )
+
+                  job_score_logs.each do |log|
+                    temp_job_id_score = conn.zscore(job_score_key, log).to_f
+
+                    conn.zremrangebyscore(
+                      job_log_key,
+                      temp_job_id_score,
+                      "(#{temp_job_id_score + 1}"
+                    )
+
+                    conn.zrem(job_score_key, log)
+                  end
+
+                  break if job_score_logs.size <= 100
+                end
+
               end
             rescue StandardError => ex
               Sidekiq.logger.error ex
