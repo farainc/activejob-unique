2c8206568b77c7ed2cad91d7ca4c21b7d2ecb030
josh.c <1102216+Ox0rigin@users.noreply.github.com>
2023-09-06 18:18:56 +0800

    Update testing projects

--- app4x/config/initializers/sidekiq.rb
+++ app4x/config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = { url: "redis://#{ENV['DC_REDIS_HOST'] || '127.0.0.1'}:6379" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = { url: "redis://#{ENV['DC_REDIS_HOST'] || '127.0.0.1'}:6379" }
 end
